a58b1d904f0027ba62ee7c2e85ead6ac6c1f9e19
Alexey Gochachilov <agochachilov@gmail.com>
2025-12-10 13:02:02 +0300

    creat constructor for class Student

--- src/SecondHomeWork/Student.java
+++ src/SecondHomeWork/Student.java
@@ -9,4 +9,11 @@ public class Student {
     private int age;
     private int course;
     private List<Book> books;
+
+    public Student(String firstName, String lastName, int age, int course) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.course = course;
+    }
 }
