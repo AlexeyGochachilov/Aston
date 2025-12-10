67f01df101e468641d7798c8b6eb63ff5dcc70e5
Alexey Gochachilov <agochachilov@gmail.com>
2025-12-10 12:56:49 +0300

    REFACTOR fields for class Book

--- src/SecondHomeWork/Book.java
+++ src/SecondHomeWork/Book.java
@@ -2,8 +2,15 @@ package SecondHomeWork;
 
 public class Book {
 
-    private String name;
-    private String author;
-    private int yearOfPublishing;
-    private int quantityOfPages;
+    private final String NAME;
+    private final String AUTHOR;
+    private final int YEAR_OF_PUBLISHING;
+    private final int QUANTITU_OF_PAGES;
+
+    public Book(String name, String author, int yearOfPublishing, int quantityOfPages) {
+        this.NAME = name;
+        this.AUTHOR = author;
+        this.YEAR_OF_PUBLISHING = yearOfPublishing;
+        this.QUANTITU_OF_PAGES = quantityOfPages;
+    }
 }
