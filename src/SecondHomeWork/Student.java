3bb24c928ab54e000b1f97970f535505a5870454
Alexey Gochachilov <agochachilov@gmail.com>
2025-12-10 13:00:02 +0300

    creat fields for class Student

--- src/SecondHomeWork/Student.java
+++ src/SecondHomeWork/Student.java
@@ -1,4 +1,12 @@
 package SecondHomeWork;
 
+import java.util.List;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int course;
+    private List<Book> books;
 }
