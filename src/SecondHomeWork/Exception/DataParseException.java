98b4e7ad2d26ed04985b08789f9851fcb4a6a37e
Alexey Gochachilov <agochachilov@gmail.com>
2025-12-11 16:46:40 +0300

    cleaning all code like Robert Martin

--- src/SecondHomeWork/Exception/DataParseException.java
+++ src/SecondHomeWork/Exception/DataParseException.java
@@ -1,11 +1,8 @@
 package SecondHomeWork.Exception;
 
 public class DataParseException extends RuntimeException {
+
     public DataParseException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public DataParseException(String message) {
-        super(message);
     }
 }
