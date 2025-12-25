df29f56412b21172d1ac414d35f4600f57756b6d
Alexey Gochachilov <agochachilov@gmail.com>
2025-12-25 19:12:54 +0300

    creat LiveLock

--- src/main/java/ru/aston/hometask/fourth/LiveLock.java
+++ src/main/java/ru/aston/hometask/fourth/LiveLock.java
@@ -1,4 +1,53 @@
 package ru.aston.hometask.fourth;
 
 public class LiveLock {
+
+    private static int counter = 0;
+    private static boolean canIncrease = true;
+    private static boolean canDecrease = false;
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(() -> {
+            while (true) {
+                synchronized (lock) {
+                    if (canIncrease) {
+                        counter++;
+                        System.out.println(counter);
+                        canIncrease = false;
+                        canDecrease = true;
+                    }
+                    if (counter >= 100) {
+                        System.out.println("The limit has been reached, resetting counter");
+                        counter = 0;
+                        canIncrease = true;
+                        canDecrease = false;
+                    }
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            while (true) {
+                synchronized (lock) {
+                    if (canDecrease) {
+                        counter--;
+                        System.out.println(counter);
+                        canDecrease = false;
+                        canIncrease = true;
+                    }
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
 }
